05aaf6d9c5ac60c48015784f3ea7b6ba2ff017cd
Bob B <bbailey@cando.com>
2017-01-04 21:06:02 +0000

    #136439993: refactor About component, add edit/view mode switching, new is bio saved in state, styling fix

--- components/About/About.js
+++ components/About/About.js
@@ -1,17 +1,48 @@
 import React from 'react';
+import s from '../Layout/Layout.css';
 
-class About extends React.Component {
+var About = React.createClass({
+  getInitialState() {
+    return { biography:this.props.biography,
+             editing:false };
+  },
+
+  startEdit() {
+
+    return this.setState({editing:true});
+  },
+
+  stopEdit() {
+    return this.setState({editing:false});
+  },
+
+  submitEdit(event) {
+    let newValue = document.getElementById("bioTextArea").value;
+    return this.setState({biography:newValue, editing:false});
+  },
+
   render() {
-    return (
+    if (this.state.editing){
+      return (
         <about>
           <h2>About</h2>
-          <button>edit</button>
+          <button className={s.topRight} onClick={this.stopEdit}>cancel</button>
+          <textarea id="bioTextArea" defaultValue={this.state.biography} />
+          <button onClick={(e)=>this.submitEdit(e)}>submit</button>
+        </about> 
+      );
+    } else {
+      return (
+        <about>
+          <h2>About</h2>
+          <button className={s.topRight} onClick={this.startEdit}>edit</button>
           <p>
-            {this.props.biography}
+            {this.state.biography || '-- no bio supplied --'}
           </p>
         </about>
-    );
+      );
+    }
   }
-}
+});
 
 export default About;
